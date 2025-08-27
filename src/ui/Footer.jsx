f81afaec1764daf9440290fda5b91681c5f015d4
Gal <galm85@gmail.com>
2025-08-27 10:14:49 +0300

    update footer style

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,7 +1,12 @@
+import Logo from '../assets/images/logo.png';
+
 function Footer() {
     return (
         <footer className="footer">
-            TeastNear
+           <div className="logo-wrapper">
+            <img src={Logo} alt="teasty near logo" />
+           </div>
+           <p className='copyrights'> Tasty Near &copy; {new Date().getFullYear()}  |  <a href="https:www.galwd.com" target='blank'> GWD</a> </p>
         </footer>
     )
 }
