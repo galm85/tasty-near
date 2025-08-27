fe60e9e5c396159ab485f9b85063db9c24e05e4c
Gal <galm85@gmail.com>
2025-08-27 10:20:50 +0300

    update total price

--- src/ui/OrderList.jsx
+++ src/ui/OrderList.jsx
@@ -12,45 +12,42 @@ function OrderList() {
     const [cartDisplay,setCartDisplay] = useState(true)
     
     return (
-        <>
         
-       
-         <aside className={cartDisplay ?  'cart-list show-cart' : 'cart-list'}>
+        <aside className={cartDisplay ?  'cart-list show-cart' : 'cart-list'}>
             <button className="hide-cart toggle-cart" onClick={()=>setCartDisplay(!cartDisplay)}>{cartDisplay ? <FaArrowLeft /> :<BsCart3 />}</button>
             
            {orderLoading && <div className="order-loading">Loading</div>}
 
             {cartDisplay && <>
+                <h3 className="cart-list__title">{user.name}'s Cart</h3>
             
-            <h3 className="cart-list__title">{user.name}'s Cart</h3>
-         
-            <div className="cart-list__items">
-                {cart.map(item => (
-                    <div className="cart-item" key={item.id}>
-                        <img className="item-image" src={item.image} alt={item.title}/>
-                        <p className="item-title">{item.title}</p>
-                        <div className="item-qty">
-                            <button onClick={()=>updateCart(item,'add')}>+</button>
-                            <span>{item.qty}</span>
-                            <button onClick={()=>updateCart(item,'less')}>-</button>
+                <div className="cart-list__items">
+                    {cart.map(item => (
+                        <div className="cart-item" key={item.id}>
+                            <img className="item-image" src={item.image} alt={item.title}/>
+                            <p className="item-title">{item.title}</p>
+                            <div className="item-qty">
+                                <button onClick={()=>updateCart(item,'add')}>+</button>
+                                <span>{item.qty}</span>
+                                <button onClick={()=>updateCart(item,'less')}>-</button>
+                            </div>
+                            <button className="item-remove" onClick={()=>removeItem(item.id)}>remove</button>
                         </div>
-                        <button className="item-remove" onClick={()=>removeItem(item.id)}>remove</button>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
 
-            <div className="cart-list__total-price">
-                Total Price: ${totalPrice.toFixed(2)}
-            </div>
+                <div className="cart-list__total-price">
+                    <p>Total Price: <span> ${totalPrice.toFixed(2)}</span></p>
+                </div>
 
-            <div className="cart-list__actions">
-                {cart.length > 0 && <button onClick={()=>createOrder(user.user_id)}>Order Now</button>}
-                {cart.length > 0 && <button onClick={()=>clearCart()}>Clear Cart</button>}
-            </div>
-            </> }
+                <div className="cart-list__actions">
+                    {cart.length > 0 && <button onClick={()=>createOrder(user.user_id)}>Order Now</button>}
+                    {cart.length > 0 && <button onClick={()=>clearCart()}>Clear Cart</button>}
+                </div>
+            </>}
          
          </aside>
-          </>
+        
          
     )
 }
